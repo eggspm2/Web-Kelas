2c925046f9ebb019a2ca36c8e11cfbc24c9ef9d3
Zansxd <134784573+eggspm2@users.noreply.github.com>
2024-10-22 14:59:47 +0700

    Kamis.jsx

--- src/components/Mapel/Kamis.jsx
+++ src/components/Mapel/Kamis.jsx
@@ -41,7 +41,7 @@ const Kamis = () => {
 				className="border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3"
 				data-aos="fade-up"
 				data-aos-duration="1000">
-				<div className="w-[50%] text-base font-medium">Matematika Tl</div>
+				<div className="w-[50%] text-base font-medium">Matematika</div>
 				<div className="flex justify-center items-center text-sm">14.25-16.05</div>
 			</div>
 		</>
